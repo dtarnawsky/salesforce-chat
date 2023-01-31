19aae04eb99d19d2d4b5df33b0a77a65edee86d0
Damian Tarnawsky <damian@ionic.io>
2023-01-31 12:41:34 -0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'my.sf.app',
+  appName: 'my-sf-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
